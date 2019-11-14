cb35dcd68f57c150c2d48fffcbaab4342d1ccbc1
zzc <983018045@qq.com>
2019-11-14 08:26:58 +0800

    导出接口

--- recharge-entity/src/main/java/com/dliberty/recharge/dto/ExportRechargeCardDto.java
+++ recharge-entity/src/main/java/com/dliberty/recharge/dto/ExportRechargeCardDto.java
@@ -2,27 +2,27 @@ package com.dliberty.recharge.dto;
 
 import com.alibaba.excel.annotation.ExcelIgnore;
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-
 
 /**
  * @author changzzc
  */
 @Data
-@EqualsAndHashCode()
 public class ExportRechargeCardDto{
 
     /**
      * 卡号
      */
     @ExcelProperty(value = "卡号" , index = 0)
+    @ColumnWidth(350)
     private String cardNo;
 
     /**
      * 密钥
      */
     @ExcelProperty(value = "密钥" , index = 1)
+    @ColumnWidth(250)
     private String secretKey;
 
     /**
